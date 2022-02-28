cad3a822cfb1821cde3248ccf6a1c57777a98c10
Alejandro Delgadillo <68914180+Slad3XXX@users.noreply.github.com>
2022-02-28 15:52:06 -0600

    first commit
    ExtensionMethods first commit

---
+++ ExtnsnMethods/ExtnsnMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExtnsnMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string PrimeraLetraMayuscula(string frase)
+        {
+            char primeraLetra = char.ToUpper(frase[0]);
+            string restoFrase = frase.Substring(1);
+            return primeraLetra + restoFrase;
+        }
+    }
+}
