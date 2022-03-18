311c75d475a3e1c961bc519c67709d08996c3b44
Alejandro Delgadillo <68914180+Slad3XXX@users.noreply.github.com>
2022-03-18 08:54:39 -0600

    first commit

---
+++ CovCont/CovCont/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace CovCont
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMyInterface<Animal> myAnimals = new GenericAnimal<Cat>();
+            
+        }
+    }
+}
+
+//Covarianza OUT
+public interface IMyInterface<out T>
+{
+    T Get();
+}
+public class Animal { }
+public class Cat : Animal { }
+public class Dog : Animal { }
+public class GenericAnimal<T> : IMyInterface<T>
+{
+    public T Get()
+    {
+        //Acá podría devolver una instancia de T.
+        return default(T);
+        
+    }
+}
+
+
+
+//Contravarianza IN
+//public class Program
+//{
+//    public static void Main()
+//    {
+//        GenericAnimal<Animal> zoo = new GenericAnimal<Animal>();
+//        zoo.Add(new Cat());
+//        zoo.Add(new Dog());
+
+//        //Aunque sean listas de gatos...
+//        //... Les puedo guardar listas de animales.
+//        IMyInterface<Cat> myCats = zoo;
+//        IMyInterface<Dog> myDogs = zoo;
+//    }
+//}
+//public interface IMyInterface<in T>
+//{
+//    void Add(T another);
+//}
+//public class Animal { }
+//public class Cat : Animal { }
+//public class Dog : Animal
+//{
+//    public void Bark()
+//    {
+//        Console.WriteLine("WOW!");
+//    }
+//}
+//public class GenericAnimal<T> : IMyInterface<T>
+//{
+//    public void Add(T another)
+//    {
+
+//    }
+//}
---
+++ Practica1Avanzdo/Practica1Avanzdo/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Practica1Avanzdo
+{
+   
+    internal class Program
+    {   
+        //Declaracion del delegado
+        public delegate void EvenNumbers(List<int> list) ;
+        public delegate void OddNumbers(List<int> list);
+        public delegate void OrderNumbersAsc(List<int> list); 
+        public delegate void OrderNumbersDes(List<int> list);
+        public delegate void DThree(int i);
+        public delegate void DPrint(string i);
+        public delegate void MaxVal(List<int> list);
+        public delegate void MinVal(List<int> list);
+        public delegate void AvgVal(List<int> list);
+        public delegate void RMS(List<int> list);
+
+
+        //Delegado Func 
+        public delegate TResult Func<in T, out TResult>(T arg);
+        static int Sum(int x, int y)
+        { return x + y;
+      
+            
+        }
+        
+
+        static void Main(string[] args)
+        {
+            //Instancia de Clase
+            Functions f = new Functions(); 
+            List<int> listExample = new List<int> { 1,2,3,4,5,6,0,7,8,9,20};
+            //Instancia del delegado
+            //Delegado nombre             delegado   (clase instanciada. metodo)
+            EvenNumbers evenNumbers = new EvenNumbers(f.Even);
+            OddNumbers oddNumbers = new OddNumbers(f.Odd);
+            OrderNumbersAsc orderNumbersAsc = new OrderNumbersAsc(f.OrderAsc);
+            OrderNumbersDes orderNumbersDes = new OrderNumbersDes(f.OrderDes);
+            DThree dThree = new DThree(f.Div3);
+            DPrint dPrint = new DPrint(f.P);
+            MaxVal maxVal = new MaxVal(f.Max);
+            MinVal minVal = new MinVal(f.Min); 
+            AvgVal avgVal = new AvgVal(f.Avg);
+            RMS rms = new RMS(f.RMS);  
+
+           Func<int, int, int> add = Sum;
+            //Delegado Action
+            Action<int> printActionDel = i => Console.WriteLine(i);
+            //Delegado Predicate
+            Predicate<string> isUpper = s => s.Equals(s.ToUpper());
+
+            //Se manda a lanzar el delegado
+            evenNumbers(listExample);
+            Console.WriteLine();
+            oddNumbers(listExample);
+            Console.WriteLine();
+            orderNumbersAsc(listExample);
+            Console.WriteLine();
+            orderNumbersDes(listExample);
+            Console.WriteLine();
+            dThree(99);
+            Console.WriteLine();
+            dPrint("hola");
+            maxVal(listExample);
+            minVal(listExample);
+            avgVal(listExample);
+            rms(listExample);
+            int result = add(121, 10);
+            Console.WriteLine(result);
+            printActionDel(2020);
+            bool resultt = isUpper("hello world!!");
+            Console.WriteLine(resultt);
+
+
+
+
+
+        }
+
+    }
+
+ 
+    public class Functions
+    {
+        public void Odd(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                if (item % 2 != 0) { Console.Write(item.ToString() + ", "); }
+            }
+        }
+        public void Even(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                if (item % 2 == 0) { Console.Write(item.ToString() + ", "); }
+            }
+        }
+        public void OrderAsc(List<int> list)
+        {
+
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.Write(item.ToString() + ", ");
+            }
+
+        }
+        public void OrderDes(List<int> list)
+        {
+
+            list.Reverse();
+            foreach (var item in list)
+            {
+                Console.Write(item.ToString() + ", ");
+            }
+
+        }
+        public void Div3(int i)
+        {
+
+
+
+            if (i % 3 == 0) { Console.Write(i + " Es mulitplo de 3"); }
+            else
+            {
+                Console.Write(i + " No es divisible entre 3");
+            }
+
+
+        }
+        public void P(string s)
+        {
+            Console.WriteLine(s);   
+
+
+        }
+        public void Max(List<int> list)
+        {
+            list.Sort();
+            var last = list.Last();
+            Console.WriteLine(last.ToString());
+
+
+
+        }
+        public void Min(List<int> list)
+        {
+            list.Sort();
+            var last = list.First();
+            Console.WriteLine(last.ToString());
+
+
+
+        }
+        public void Avg(List<int> list)
+        {
+            list.Sort();
+            var last = list.Average();
+            Console.WriteLine(last.ToString());
+
+
+
+        }
+        public void RMS(List<int> list)
+        {
+            var n = 0;
+            foreach (var item in list)
+            {
+                n += item ^ 2;
+
+            }
+
+            var res = Math.Sqrt(n / list.Count);
+            Console.WriteLine(res);
+            
+        }
+    }
+
+   
+
+   
+
+
+
+
+
+
+
+}
